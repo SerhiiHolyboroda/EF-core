3e9365d754164081c8a1432915c6d6150057fe34
Serhii <nosferatugamevampire@gmail.com>
2023-05-06 19:52:37 +0300

    adding files

---
+++ efCore/src/OrderContext.cs
@@ -0,0 +1,28 @@
+﻿using efCore.src;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace efCore.src
+{
+
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-A8R3DHS;Database=master;Trusted_Connection=True;");
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().HasKey(o => o.ord_id);
+        }
+    }
+
+
+}
---
+++ efCore/src/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace efCore.src
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+          //  using (var context = new OrderContext())
+         //   {
+          //      DateTime startDate = DateTime.Now.AddYears(-1);
+          //      var orders = context.Orders.Where(o => o.ord_datetime >= startDate).ToList();
+
+          //      foreach (var order in orders)
+          //      {
+         //           Console.WriteLine("Order ID: " + order.ord_id);
+          //          Console.WriteLine("Order Date: " + order.ord_datetime);
+          //          Console.WriteLine("Analysis ID: " + order.ord_an);
+          //          Console.WriteLine("---------------------------");
+          //      }
+          //  }
+
+
+
+            int ordId = new Random().Next(1, 1000000);
+            OrderRepository.CreateNewOrder(ordId);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            OrderRepository.UpdateOrder(ordId);
+            await Task.Delay(2000);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            OrderRepository.DeleteOrder(ordId);
+            await Task.Delay(3000);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+    }
+ 
